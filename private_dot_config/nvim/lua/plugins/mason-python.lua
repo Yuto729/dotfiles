890544267ec84d6301da1398b16a178793dc3c38
mitomi <mitomi.yuuto2003@gmail.com>
2026-02-09 23:31:21 +0900

    Revert "nvim: enable mypy linting for Python"
    This reverts commit 17e5cc9f8cc0a1a0a3a6f4e5effab2b152b576a5.

--- private_dot_config/nvim/lua/plugins/mason-python.lua
+++ private_dot_config/nvim/lua/plugins/mason-python.lua
@@ -6,7 +6,6 @@ return {
         "basedpyright",
         "ruff",
         "debugpy",
-        "mypy",
       },
     },
   },
--- private_dot_config/nvim/lua/plugins/nvim-lint.lua
+++ private_dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -1,22 +1,4 @@
 return {
   "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
-
-    -- Python用にmypyを設定
-    lint.linters_by_ft = {
-      python = { "mypy" },
-    }
-
-    -- 保存時と入力停止後にlintを実行
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
-  end,
+  enabled = false, -- nvim-lintを完全に無効化
 }
