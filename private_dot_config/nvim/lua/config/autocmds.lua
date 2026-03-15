2d8200195435dcc80eb9ce395f2c3503dd79fb7b
mitomi <mitomi.yuuto2003@gmail.com>
2026-03-15 22:29:51 +0900

    markdownの横幅をwrapされるようにした

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -7,5 +7,12 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 
 -- Markdownでのスペルチェックを無効化（日本語対応）
+-- lazyvim_wrap_spell グループはwrapも設定するため丸ごと削除せず、spellだけ無効化する
 -- 参考: https://clameyes.com/posts/lazyvim-underline-red-wave-remove-japanese
-vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "gitcommit" },
+  callback = function()
+    vim.opt_local.spell = false
+    vim.opt_local.wrap = true
+  end,
+})
